0ff8e43bde57660077af34172537b1d6a39890a9
Hyunjun <leehyunjune11@naver.com>
2026-02-05 18:44:02 +0900

    style main padding

--- codeit_test/app/page.tsx
+++ codeit_test/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
 
   return (
     <div>
-      <section className="flex gap-3 md:gap-5 py-8">
+      <section className="flex gap-3 md:gap-5 py-8 px-8">
         <input
           type="text"
           value={inputValue}
